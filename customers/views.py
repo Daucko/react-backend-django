8a18c6ce98369e617dc4fe366cffc68647acc620
Daucko <dauda.a.tijani@gmail.com>
2024-10-31 11:01:17 +0000

    Deleted and recreated the .venv

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404, JsonResponse
 from customers.models import Customer
 from customers.serializers import CustomerSerializer, UserSerializer
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes 
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
